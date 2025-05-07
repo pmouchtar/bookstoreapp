050801a0ababf9375a3c4321d1c2a80329969ba5
petros <pmouchtar@csd.auth.com>
2025-05-07 12:22:01 +0300

    corrected price range problem and title-author <=255 characters problem found in testing

--- src/main/java/com/petros/bookstore/dto/BookRequest.java
+++ src/main/java/com/petros/bookstore/dto/BookRequest.java
@@ -2,10 +2,7 @@ package com.petros.bookstore.dto;
 
 import com.petros.bookstore.model.Book;
 import com.petros.bookstore.model.enums.Genre;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,21 +15,23 @@ import lombok.NoArgsConstructor;
 public class BookRequest {
 
     @NotBlank(message = "Title is required")
+    @Size(max = 255, message = "Title must be at most 255 characters")
     private String title;
 
     @NotBlank(message = "Author is required")
+    @Size(max = 255, message = "Author must be at most 255 characters")
     private String author;
 
     @Size(max = 500, message = "Description must be at most 500 characters")
     private String description;
 
     @Min(value = 0, message = "Price must be >= 0")
+    @Digits(integer = 5, fraction = 2, message = "Integer part must be <=5 digits, Decimal part must be <=2 digits")
     private Float price;
 
     @Min(value = 0, message = "Availability must be >= 0")
     private Integer availability;
 
     @NotNull(message = "Genre is required")
-    @NotBlank(message = "Genre cannot be empty")
     private Genre genre;
 }
